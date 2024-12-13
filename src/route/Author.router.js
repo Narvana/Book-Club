09999516814a23d27d02ad1750739fd4876a1311
Narvana <adhikarijagdish3@gmail.com>
2024-12-13 17:15:25 +0530

    Book Club Author Router update

--- src/route/Author.router.js
+++ src/route/Author.router.js
@@ -4,8 +4,9 @@ const router=express.Router()
 
 const {GetAuthor,UpdateAuthor}=require('../controller/author.controller');
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
+const {verify}=require('../middleware/verifyToken');
  
 router.get('/Get',GetAuthor);
-router.put('/Update',upload.single('authorImage'),UpdateAuthor);
+router.put('/Update',verify(['ADMIN']),upload.single('authorImage'),UpdateAuthor);
 
 module.exports=router
