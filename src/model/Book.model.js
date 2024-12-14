5159462ecd414eddfe06dfffacbc32d64586da2c
Narvana <adhikarijagdish3@gmail.com>
2024-12-14 14:39:43 +0530

    Book Model Update Title Uniue and BookVote Added

--- src/model/Book.model.js
+++ src/model/Book.model.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 const BookSchema=new mongoose.Schema({
     title: {
         type:String,
+        unique:true,
         required:[true,'Book Title is required']
     },
     authorID:{
@@ -31,6 +32,33 @@ const BookSchema=new mongoose.Schema({
         type:String,
         required:[true,'Book Cover Image is required']
     }, 
+    BookVote: {
+        type: [String]
+        // validate: [
+        //     {
+        //         validator: function (emails) {
+        //             // Check if array contains only one email
+        //             return emails.length === 1;
+        //         },
+        //         message: 'BookVote can only contain one email at a time.',
+        //     },
+        //     {
+        //         validator: function (emails) {
+        //             // Check if all emails are unique
+        //             return new Set(emails).size === emails.length;
+        //         },
+        //         message: 'BookVote must contain unique email addresses.',
+        //     },
+        //     {
+        //         validator: function (emails) {
+        //             // Check if the email is valid
+        //             return emails.every(email => validator.isEmail(email));
+        //         },
+        //         message: 'BookVote accepts only valid email addresses.',
+        //     },
+        // ],
+    },
+    
 })
 
 const Book = mongoose.model('Book',BookSchema);
