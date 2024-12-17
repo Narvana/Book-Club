1161e49cf0c44e5fbe87ee969061bd1fe15120bd
Narvana <adhikarijagdish3@gmail.com>
2024-12-17 16:15:38 +0530

    Create Admin Book Vote and RemoveBookVote router added

--- src/route/BookVote.route.js
+++ src/route/BookVote.route.js
@@ -2,16 +2,20 @@ const express=require('express')
 
 const router=express.Router()
 
-const {Vote,getBookVote,getBookVotePercent}=require('../controller/BookVote.controller');
+const {Vote,getBookVote,getBookVotePercent,CreateAdminBookVote,RemoveBookVote}=require('../controller/BookVote.controller');
 // const defineRole=require('../middleware/role/defineRole');
 // const verifyRole=require('../middleware/role/verifyRole')
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
 const {verify}=require('../middleware/verifyToken')
+
+router.post('/List/Add/Admin',verify(['ADMIN']),CreateAdminBookVote)
  
 router.post('/Add',Vote);
 
 router.get('/Count',getBookVote);
 
 router.get('/Percent',getBookVotePercent);
+
+router.delete('/Delete/Admin',verify(['ADMIN']),RemoveBookVote);
 
 module.exports=router
