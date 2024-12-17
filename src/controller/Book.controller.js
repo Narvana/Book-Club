f16483893f4112560121d70aa68270fe2c850a8a
Narvana <adhikarijagdish3@gmail.com>
2024-12-17 12:14:22 +0530

    Book controller updated

--- src/controller/Book.controller.js
+++ src/controller/Book.controller.js
@@ -133,7 +133,8 @@ const GetBooks=async(req,res,next)=>{
 
 const UpdateBook=async(req,res,next)=>{
     const id=req.query.BookID;
-    try {
+    try 
+    {
         if(!id)
         {
             return next(ApiError(400,'Please provide Book ID'));
@@ -258,160 +259,156 @@ const GetAuthor=async(req,res,next)=>{
 
 //     );
 // }
-const BookVote = async (req, res, next) => {
-    try {
-        const email = req.query.Email;
-        const id = req.query.BookID;
+// const BookVote = async (req, res, next) => {
+//     try {
+//         const email = req.query.Email;
+//         const id = req.query.BookID;
 
-        if (!id) {
-            return next(ApiError(400, 'Choose the book you want to vote for.'));
-        }
+//         if (!id) {
+//             return next(ApiError(400, 'Choose the book you want to vote for.'));
+//         }
 
-        if (!email) {
-            return next(ApiError(400, 'Enter your email to vote.'));
-        }
+//         if (!email) {
+//             return next(ApiError(400, 'Enter your email to vote.'));
+//         }
 
-        // Validate email format
-        if (!validator.isEmail(email)) {
-            return next(ApiError(400, 'Enter a valid email address.'));
-        }
+//         // Validate email format
+//         if (!validator.isEmail(email)) {
+//             return next(ApiError(400, 'Enter a valid email address.'));
+//         }
 
-        // Find the book and ensure uniqueness of email in BookVote array
-        const book = await Book.findById(id);
+//         // Find the book and ensure uniqueness of email in BookVote array
+//         const book = await Book.findById(id);
 
-        if (!book) {
-            return next(ApiError(404, 'Book not found.'));
-        }
-        if (book.BookVote.includes(email)) {
-            return next(ApiError(400, 'You have already voted for this book.'));
-        }
+//         if (!book) {
+//             return next(ApiError(404, 'Book not found.'));
+//         }
+//         if (book.BookVote.includes(email)) {
+//             return next(ApiError(400, 'You have already voted for this book.'));
+//         }
 
-        book.BookVote.push(email);
+//         book.BookVote.push(email);
 
-        await book.save();
+//         await book.save();
 
-        return next(ApiSuccess(200,book,`${email} voted for ${book.title}`))
+//         return next(ApiSuccess(200,book,`${email} voted for ${book.title}`))
 
-    } catch (error) {
-        console.log(
-            {
-                'Internal Serve Error, ' : error.message,
-                error
-            }
-        );
+//     } catch (error) {
+//         console.log(
+//             {
+//                 'Internal Serve Error, ' : error.message,
+//                 error
+//             }
+//         );
 
-        return next(ApiError(500, `An error occurred while voting for the book ${error.message}`));
-    }
-};
+//         return next(ApiError(500, `An error occurred while voting for the book ${error.message}`));
+//     }
+// };
 
-const TotalVoteData=async(req,res,next)=>{
+// const TotalVoteData=async(req,res,next)=>{
 
-    // const book=await Book.find();
+//     // const book=await Book.find();
 
-    // let TotalVote=0
+//     // let TotalVote=0
 
-    // book.map((Book)=>{
-    //     vote=Book.BookVote.length
-    //     TotalVote += vote;
-    // })
+//     // book.map((Book)=>{
+//     //     vote=Book.BookVote.length
+//     //     TotalVote += vote;
+//     // })
 
-    // const BookVote=await Book.aggregate([
-    //     {
-    //         $lookup:{
-    //             from:'authors',
-    //             localField: 'authorID',
-    //             foreignField: '_id',
-    //             as: 'AuthorDetails'
-    //         }
-    //     },
-    //     {
-    //         $unwind:{
-    //             path:'$AuthorDetails'
-    //         }
-    //     },
-    //     {
-    //         $project:{
-    //             title:1,
-    //             BookCover:1,
-    //             Author: '$AuhtorDetails.authorName',
-    //             TotalVote: { $size: "$BookVote" },
-    //             VotePercent: '$BookVote'/TotalVote
-    //         }
-    //     }
-    // ])
+//     // const BookVote=await Book.aggregate([
+//     //     {
+//     //         $lookup:{
+//     //             from:'authors',
+//     //             localField: 'authorID',
+//     //             foreignField: '_id',
+//     //             as: 'AuthorDetails'
+//     //         }
+//     //     },
+//     //     {
+//     //         $unwind:{
+//     //             path:'$AuthorDetails'
+//     //         }
+//     //     },
+//     //     {
+//     //         $project:{
+//     //             title:1,
+//     //             BookCover:1,
+//     //             Author: '$AuhtorDetails.authorName',
+//     //             TotalVote: { $size: "$BookVote" },
+//     //             VotePercent: '$BookVote'/TotalVote
+//     //         }
+//     //     }
+//     // ])
 
-    // return next(ApiSuccess(200,{
-    //     TotalVote,
-    //     BookVote
-    // },'Votes'))
-    const BookVote = await Book.aggregate([
-        {
-            $lookup: {
-                from: 'authors', // Join with authors collection
-                localField: 'authorID', // Field in Book collection
-                foreignField: '_id', // Field in authors collection
-                as: 'AuthorDetails', // Output array field
-            },
-        },
-        {
-            $unwind: {
-                path: '$AuthorDetails', // Flatten AuthorDetails array
-            },
-        },
-        {
-            $project: {
-                title: 1,
-                BookCover: 1,
-                Author: '$AuthorDetails.authorName', // Fixed typo
-                TotalVote: { $size: '$BookVote' }, // Total votes for each book
-            },
-        },
-        {
-            $group: {
-                _id: null, // Group all documents together
-                TotalVotesAcrossBooks: { $sum: '$TotalVote' }, // Sum of all votes
-                Books: { $push: '$$ROOT' }, // Push the book details
-            },
-        },
-        {
-            $unwind: {
-                path: '$Books', // Unwind Books array for further processing
-            },
-        },
-        {
-            $project: {
-                _id: '$Books._id',
-                title: '$Books.title',
-                BookCover: '$Books.BookCover',
-                Author: '$Books.Author',
-                TotalVote: '$Books.TotalVote',
-                VotePercent: {
-                    $cond: {
-                        if: { $gt: ['$TotalVotesAcrossBooks', 0] }, // Avoid division by zero
-                        then: {
-                            $multiply: [
-                                { $divide: ['$Books.TotalVote', '$TotalVotesAcrossBooks'] },
-                                100, // Convert to percentage
-                            ],
-                        },
-                        else: 0,
-                    },
-                },
-            },
-        },
-    ]);
+//     // return next(ApiSuccess(200,{
+//     //     TotalVote,
+//     //     BookVote
+//     // },'Votes'))
+//     const BookVote = await Book.aggregate([
+//         {
+//             $lookup: {
+//                 from: 'authors', // Join with authors collection
+//                 localField: 'authorID', // Field in Book collection
+//                 foreignField: '_id', // Field in authors collection
+//                 as: 'AuthorDetails', // Output array field
+//             },
+//         },
+//         {
+//             $unwind: {
+//                 path: '$AuthorDetails', // Flatten AuthorDetails array
+//             },
+//         },
+//         {
+//             $project: {
+//                 title: 1,
+//                 BookCover: 1,
+//                 Author: '$AuthorDetails.authorName', // Fixed typo
+//                 TotalVote: { $size: '$BookVote' }, // Total votes for each book
+//             },
+//         },
+//         {
+//             $group: {
+//                 _id: null, // Group all documents together
+//                 TotalVotesAcrossBooks: { $sum: '$TotalVote' }, // Sum of all votes
+//                 Books: { $push: '$$ROOT' }, // Push the book details
+//             },
+//         },
+//         {
+//             $unwind: {
+//                 path: '$Books', // Unwind Books array for further processing
+//             },
+//         },
+//         {
+//             $project: {
+//                 _id: '$Books._id',
+//                 title: '$Books.title',
+//                 BookCover: '$Books.BookCover',
+//                 Author: '$Books.Author',
+//                 TotalVote: '$Books.TotalVote',
+//                 VotePercent: {
+//                     $cond: {
+//                         if: { $gt: ['$TotalVotesAcrossBooks', 0] }, // Avoid division by zero
+//                         then: {
+//                             $multiply: [
+//                                 { $divide: ['$Books.TotalVote', '$TotalVotesAcrossBooks'] },
+//                                 100, // Convert to percentage
+//                             ],
+//                         },
+//                         else: 0,
+//                     },
+//                 },
+//             },
+//         },
+//     ]);
     
-    return next(
-        ApiSuccess(200, {
-            TotalVotes: BookVote.reduce((sum, book) => sum + book.TotalVote, 0),
-            BookVote,
-        }, 'Votes')
-    );
-    
-
-}
-
-
+//     return next(
+//         ApiSuccess(200, {
+//             TotalVotes: BookVote.reduce((sum, book) => sum + book.TotalVote, 0),
+//             BookVote,
+//         }, 'Votes')
+//     );
+// }
 
 module.exports={
     EnterBook,
@@ -419,6 +416,6 @@ module.exports={
     RemoveBook,
     GetAuthor,
     UpdateBook,
-    BookVote,
-    TotalVoteData
+    // BookVote,
+    // TotalVoteData
 }
