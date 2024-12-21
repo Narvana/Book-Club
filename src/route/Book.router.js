34c8f628d0300a04248e408541c8f3a48c1b0f3c
Narvana <adhikarijagdish3@gmail.com>
2024-12-21 17:47:59 +0530

    Book route Updated

--- src/route/Book.router.js
+++ src/route/Book.router.js
@@ -2,7 +2,7 @@ const express=require('express')
 
 const router=express.Router()
 
-const {EnterBook,GetBooks,RemoveBook,UpdateBook,BookVote,TotalVoteData}=require('../controller/Book.controller');
+const {EnterBook,GetBooks,RemoveBook,UpdateBook}=require('../controller/Book.controller');
 // const defineRole=require('../middleware/role/defineRole');
 // const verifyRole=require('../middleware/role/verifyRole')
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
