042581640b1fc10348d65cf11a5b0493246262fa
Narvana <adhikarijagdish3@gmail.com>
2024-12-21 17:44:46 +0530

    Book Discussion Model Added

---
+++ src/model/BookDiscussion.model.js
@@ -0,0 +1,58 @@
+const mongoose=require('mongoose');
+const validator=require('validator');
+
+const DiscussionSchema= new mongoose.Schema({
+    BookID:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: [true,'Book ID is required']
+    },
+    DiscussionDate:{
+        type:Date,
+        required: [true,'Discussion Date is required'],
+        validate:
+        {
+            validator:function(value){
+                return value >= new Date();
+            },
+            message: 'Disscussion Date should be equal to or later than today Date.'
+        }
+    },
+    DiscussionStartTime: {
+        type: String,
+        required: [true,'Discussion Start Time is required'],
+        validate:
+        {
+            validator:function(value){
+                return /^([1-9]|1[0-2]):[0-5][0-9]\s(AM|PM)$/.test(value);
+            },
+            message: 'Invalid time format. Please use "hh:mm AM/PM".'
+        }
+    },
+    DiscussionEndTime: {
+        type: String, 
+        required: [true,'Discussion End Time is required'],
+        validate:
+        [
+            {
+                validator: function (value) {
+                    return /^([1-9]|1[0-2]):[0-5][0-9]\s(AM|PM)$/.test(value);
+                },
+                message: 'Invalid time format. Please use "hh:mm AM/PM".'
+            },
+            // {
+            //     validator: function (value) 
+            //     {
+            //         const startTime = parseTime(this.DiscussionStartTime);
+            //         const endTime = parseTime(value);
+            //         return startTime < endTime; // Ensure start time is before end time
+            //     },
+            //     message: 'Discussion Start Time must be earlier than Discussion End Time.'
+            // }
+        ]
+    }
+})
+
+const BookDiscussion= mongoose.model('BookDiscussion',DiscussionSchema);
+
+module.exports = BookDiscussion;
