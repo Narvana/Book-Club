97540e27d40373bac78fe9b4ef3e8e462ae765a2
Narvana <adhikarijagdish3@gmail.com>
2024-12-21 17:47:29 +0530

    Filter route Updated discussion

--- src/route/Filter.route.js
+++ src/route/Filter.route.js
@@ -2,13 +2,14 @@ const express=require('express')
 
 const router=express.Router()
 
-const {updatepoll,updateMonthYear,getFilter}=require('../controller/Filter.controller');
+const {updatepoll,updateMonthYear,updateDiscussion,getFilter}=require('../controller/Filter.controller');
 
 const {verify}=require('../middleware/verifyToken')
 
 router.post('/Poll',verify(['ADMIN']),updatepoll);
 
 router.post('/MonthYear',verify(['ADMIN']),updateMonthYear);
+router.post('/Discussion',verify(['ADMIN']),updateDiscussion);
 
 router.get('/Get',verify(['ADMIN']),
 getFilter); 
