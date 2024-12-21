f5393e10f2f5cae06be0d08033224d3b96f99fa1
Narvana <adhikarijagdish3@gmail.com>
2024-12-21 17:47:13 +0530

    Filter Controller Updated discussion

--- src/controller/Filter.controller.js
+++ src/controller/Filter.controller.js
@@ -45,7 +45,7 @@ const updatepoll=async(req,res,next)=>{
             const errorMessages = Object.values(error.errors).map(error => error.message);
             return next(ApiError(500,errorMessages[0]));            
         }
-        return next(ApiError(500, `An error occurred while voting for the book ${error.message}`)); 
+        return next(ApiError(500, `An error occurred ${error.message}`)); 
     }
 }
 
@@ -71,8 +71,7 @@ const updateMonthYear=async(req,res,next)=>{
         return next(ApiError(400, `Please Provide Month and year`));     
     }
 
-    // return console.log(matchCondition);
-    
+    // return console.log(matchCondition)
 
     try {
         const checkFilter=await Filter.findOne();    
@@ -109,6 +108,51 @@ const updateMonthYear=async(req,res,next)=>{
     }
 }
 
+const updateDiscussion=async(req,res,next)=>{
+    const {discussion} = req.query;
+
+    if(!discussion)
+    {
+        return next(ApiError(400,'Please provide Discussion status'))    
+    }
+
+    try {      
+        const checkFilter=await Filter.findOne();
+        // return console.log(checkFilter);
+        
+
+        if(!checkFilter)
+        {
+            const createFilter=new Filter({
+                discussion
+            })
+            const filter=await createFilter.save();
+            return next(ApiSuccess(200,filter,'Filter created'));
+        }
+        else if(checkFilter)
+        {
+
+            checkFilter.discussion = discussion
+            await checkFilter.save();
+            return next(ApiSuccess(200, checkFilter, 'Filter updated'));
+        }
+            
+    } catch (error) {
+        console.log(
+            {
+                'Internal Serve Error, ' : error.message,
+                error
+            }
+        );
+        if(error.name === 'ValidationError')
+        {
+            const errorMessages = Object.values(error.errors).map(error => error.message);
+            return next(ApiError(500,errorMessages[0]));            
+        }
+        return next(ApiError(500, `An error occurred ${error.message}`)); 
+    }
+}
+
 const getFilter=async(req,res,next)=>{
     try {
         const checkFilter=await Filter.findOne();    
@@ -136,5 +180,6 @@ const getFilter=async(req,res,next)=>{
 module.exports={
     updatepoll,
     updateMonthYear,
+    updateDiscussion,
     getFilter
 }
