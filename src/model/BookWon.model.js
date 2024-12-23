6b3c2c81c950db5feec5e681db5bb1e8b53f102a
Narvana <adhikarijagdish3@gmail.com>
2024-12-23 14:52:38 +0530

    Book Won Model updated BookID

--- src/model/BookWon.model.js
+++ src/model/BookWon.model.js
@@ -1,23 +1,23 @@
 const mongoose=require('mongoose');
 
 const BookWonSchema = new mongoose.Schema({
-    // BookID: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Book',
-    //     required:[true,'Book ID is required']
+    BookID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required:[true,'Book ID is required']
+    },
+    // Booktitle:{
+    //     type: String,
+    //     required: [true, 'Book title is required']
     // },
-    Booktitle:{
-        type: String,
-        required: [true, 'Book title is required']
-    },
-    BookCover:{
-        type: String,
-        required: [true, 'Book Cover is required']
-    },
-    AuthorName:{
-        type: String,
-        required: [true, 'Author Name is required']
-    },
+    // BookCover:{
+    //     type: String,
+    //     required: [true, 'Book Cover is required']
+    // },
+    // AuthorName:{
+    //     type: String,
+    //     required: [true, 'Author Name is required']
+    // },
     Month:{
         type:String,   
         required: [true, 'Month is required']
