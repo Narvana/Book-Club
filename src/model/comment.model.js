48126181cf7f44f5db01688b4cd2f0f0ba500641
Narvana <adhikarijagdish3@gmail.com>
2024-12-23 16:38:50 +0530

    Name added in Model

--- src/model/comment.model.js
+++ src/model/comment.model.js
@@ -1,16 +1,27 @@
 const mongoose=require('mongoose');
-const Book = require('./Book.model');
+const validator=require('validator');
 
 const CommentSchema = new mongoose.Schema({
 
     BookID:{
         type: mongoose.Schema.Types.ObjectId,
-        ref:Book,
+        ref:'Book',
         required:[true,'Book ID is required']
     },    
+    name:{
+        type:String,
+        required:[true,'Name is required to comment']
+    },
     email:{
         type:String,
-        required:[true,'Email is required to comment']
+        required:[true,'Email is required to comment'],
+        validate:{
+                validator(value){
+                    if(!validator.isEmail(value)){
+                       throw new Error("Write a Valid Email")
+                    }
+                }
+            }
     },
     comment:{
         type:String,
