debc2f1da6c11d4bb30b72c2eb3d954575927454
Narvana <adhikarijagdish3@gmail.com>
2024-12-24 00:56:50 +0530

    Book Won router updated Theme Added

--- src/route/BookWon.route.js
+++ src/route/BookWon.route.js
@@ -2,13 +2,16 @@ const express=require('express')
 
 const router=express.Router()
 
-const {CheckWinner, GetBookWinner}=require('../controller/BookWon.controller');
+const {CheckWinner, GetBookWinner, UpdateBookWonTheme}=require('../controller/BookWon.controller');
 
 const {verify}=require('../middleware/verifyToken')
 
 router.post('/Check/Admin',verify(['ADMIN']),CheckWinner)
 
 router.get('/Get/Winner/Admin',GetBookWinner)
+
+router.put('/Update/Theme',verify(['ADMIN']),UpdateBookWonTheme)
+
  
 
 module.exports=router
