3cdcf69f8f5a6f04638b6e538c9e25cb9cdead7a
Narvana <adhikarijagdish3@gmail.com>
2024-12-23 22:33:49 +0530

    Comment route Delete

--- src/route/comment.route.js
+++ src/route/comment.route.js
@@ -1,14 +1,15 @@
-const express=require('express')
+const express=require('express');
 
-const router=express.Router()
+const router=express.Router();
 
-const {WriteComment,GetComments}=require('../controller/Comment.controller');
+const {WriteComment,GetComments,DeleteComment}=require('../controller/Comment.controller');
 
-// const {verify}=require('../middleware/verifyToken')
+const {verify}=require('../middleware/verifyToken');
 
-router.post('/Write',WriteComment)
+router.post('/Write',WriteComment);
 
-router.get('/Get',GetComments)
+router.get('/Get',GetComments);
+
+router.delete('/Remove',verify(['ADMIN']),DeleteComment);
  
- 
-module.exports=router
+module.exports=router;
