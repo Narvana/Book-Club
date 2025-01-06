c3faaafebe63ba6f6112644dff8e9d5d184a92cf
Narvana <adhikarijagdish3@gmail.com>
2025-01-06 16:22:49 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -2,33 +2,39 @@ require('dotenv').config();
 require('./database/BC.database');
 
 const express = require('express')
-const app=express()
+const app=express();
 const helmet = require('helmet')
 const cors=require('cors')
 const cookieParser=require('cookie-parser')
 const mongoose = require('mongoose');
-const multer=require('multer');
+const multer = require('multer');
+const http = require('http');  
+// const hpp = require('hpp');
 
-const port = process.env.PORT || 1000;
 
 // const csrf = require('csurf');
 // const csrfProtection = csrf({ cookie: true });
+const port = process.env.PORT || 1000;
 
+// const cookieSignature = require('cookie-signature');
 
-const AuthRoute=require('./route/auth.router');
-const BookRoute = require('./route/Book.router');
-const AuthorRoute = require('./route/Author.router');
-const BookVoteRoute= require('./route/BookVote.route');
-const BookWonRoute= require('./route/BookWon.route');
-const FilterRoute = require('./route/Filter.route');
-const DiscussionRoute=require('./route/BookDiscussion.route');
-const CommentRoute=require('./route/comment.route');
-const NewsLetterRoute=require('./route/NewsLetter.router');
-const ContactUserRoute=require('./route/ContactUser.router');
+const CookieSecret=process.env.COOKIE_SECRET;
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cookieParser(CookieSecret));
 
 app.use(helmet());
-// app.use(csrfProtection);
+// app.use(helmet.xssFilter()) // no use if i'm using sanitizeMiddle
+app.use(helmet.hsts({
+    maxAge: 31536000, // One year
+    includeSubDomains: true,
+    preload: true,
+}));
+// app.use(helmet.frameguard({ action: 'sameorigin' }));
+
+
 app.use(cors({
     origin: 
     [
@@ -41,11 +47,24 @@ app.use(cors({
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
     allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+})); 
 
-app.use(cookieParser());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.get('/clear-cookie', (req, res) => {
+//     res.clearCookie('accessToken');
+//     res.send('Cookie has been cleared!');
+// });
+ 
+const AuthRoute=require('./route/auth.router');
+const BookRoute = require('./route/Book.router');
+const AuthorRoute = require('./route/Author.router');
+const BookVoteRoute= require('./route/BookVote.route');
+const BookWonRoute= require('./route/BookWon.route');
+const FilterRoute = require('./route/Filter.route');
+const DiscussionRoute=require('./route/BookDiscussion.route');
+const CommentRoute=require('./route/comment.route');
+const NewsLetterRoute=require('./route/NewsLetter.router');
+const ContactUserRoute=require('./route/ContactUser.router');
+const PodcastRoute=require('./route/Podcast.router');
 
 app.use('/api/auth',AuthRoute);
 app.use('/api/Book',BookRoute);
@@ -57,6 +76,7 @@ app.use('/api/Discussion',DiscussionRoute);
 app.use('/api/Comment',CommentRoute);
 app.use('/api/NewsLetter',NewsLetterRoute);
 app.use('/api/Contact/User',ContactUserRoute);
+app.use('/api/Podcast',PodcastRoute);
 
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) 
@@ -71,7 +91,6 @@ app.use((err, req, res, next) => {
               });
         }
     } 
-    
     if (err.message === 'Invalid file type. Only JPEG, PNG, and GIF files are allowed') 
     {
             return  res.status(400).json({
@@ -94,7 +113,6 @@ app.use((err, req, res, next) => {
           message,
         });    
     }
-
 });
 
 
@@ -112,16 +130,13 @@ app.get('/test/database',(req,res)=>{
     }
 })
 
-// app.use('/',(req,res)=>{
-//     res.send('hello')
-// })
+const server = http.createServer(app);
+server.timeout = 60000;  // Set timeout to 1 minutes
 
-// const sslServer=https.createServer({
-//     key:fs.readFileSync(path.join(__dirname,'cert','ryans-key.pem')),
-//     cert:fs.readFileSync(path.join(__dirname, 'cert', 'ryans-cert.pem')),
-// },app)
-// sslServer.
- 
-app.listen(port,()=>{
-    console.log(`Secure Connection with port ${port}`)
-})
+server.listen(port, () => {
+  console.log(`Secure Connection with port ${port}`);
+});
+
+// app.listen(port,()=>{
+//     console.log(`Secure Connection with port ${port}`)
+// })
