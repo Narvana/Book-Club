50544c5ad909c10675f5daa86d4c92dfc8574339
Narvana <adhikarijagdish3@gmail.com>
2025-01-06 17:26:38 +0530

    podcast router updated latest added

--- src/route/Podcast.router.js
+++ src/route/Podcast.router.js
@@ -20,13 +20,38 @@ const podcastValidationRules = [
         .withMessage('DateTime must be a valid date.'),
     body('videoLink')
         .isURL()
-        .withMessage('Video link must be a valid URL.')
+        .withMessage('Video link must be a valid URL.'),
+    body('latest')
+         .optional()
+        .isBoolean()
+        .withMessage('latest must be either true or false.')
 ];
 
 
 router.post('/Add',verify(['ADMIN']),...podcastValidationRules,EnterPodcast);
 router.get('/Get',GetPodcast);
-router.put('/Update',verify(['ADMIN']),...podcastValidationRules,UpdatePodcast);
+
+router.put('/Update',verify(['ADMIN']),
+[
+    body('title')
+    .optional()
+    .isString().withMessage('Title must be a string.')
+    .isLength({ min: 3, max: 100 }).withMessage('Title must be between 3 and 100 characters.'),
+    body('description')
+    .optional()
+    .isString().withMessage('Description must be a string.')
+    .isLength({ min: 10 }).withMessage('Description must be at least 10 characters.'),
+    body('dateTime')
+    .optional()
+    .isISO8601().withMessage('DateTime must be a valid date.'),
+    body('videoLink')
+    .optional()
+    .isURL().withMessage('Video link must be a valid URL.'),
+    body('latest')
+         .optional()
+        .isBoolean()
+        .withMessage('latest must be either true or false.')
+],UpdatePodcast);
 router.delete('/Remove',verify(['ADMIN']),RemovePodcast); 
 
 module.exports=router
