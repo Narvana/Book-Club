8867b43f0a8cd548896075e6f72f20cef3c4842c
Narvana <adhikarijagdish3@gmail.com>
2025-01-06 16:18:31 +0530

    podcast model added

---
+++ src/model/podcast.model.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+const validator = require('validator');
+
+const podcastSchema= new mongoose.Schema({
+    title:{
+        type: String,
+        required:true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    dateTime: {
+        type: Date, 
+        required: true
+    },
+    videoLink: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+(\.[a-zA-Z]{2,})+)(\/[^\s]*)?$/;
+                return urlRegex.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
+    }
+});
+
+const Podcast = mongoose.model('Podcast',podcastSchema);
+
+module.exports=Podcast;
+
